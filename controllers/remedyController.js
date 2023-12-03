fcaeb6a355edaadfae38969448d4e41ca764cc5f
Victor Santos <victor.santos@venetoinvest.com.br>
2023-12-03 18:41:28 -0300

    Ajuste para depurar o erro

--- controllers/remedyController.js
+++ controllers/remedyController.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
     const remedies = await prisma.remedy.findMany();
     res.json(remedies);
   } catch (error) {
-    res.status(500).json({ error: 'Erro ao buscar os medicamentos' });
+    res.status(500).json({ error: error });
   }
 });
 
@@ -21,11 +21,11 @@ router.get('/:id', async (req, res) => {
       where: { remedy_id: parseInt(id) },
     });
     if (!remedy) {
-      return res.status(404).json({ error: 'Medicamento não encontrado' });
+      return res.status(404).json({  error: error });
     }
     res.json(remedy);
   } catch (error) {
-    res.status(500).json({ error: 'Erro ao buscar o medicamento' });
+    res.status(500).json({  error: error });
   }
 });
 
@@ -48,7 +48,7 @@ router.post('/', async (req, res) => {
     res.status(201).json(newRemedy);
   } catch (error) {
     console.log(error)
-    res.status(500).json({ error: 'Erro ao cadastrar o medicamento' });
+    res.status(500).json({  error: error });
   }
 });
 
@@ -71,7 +71,7 @@ router.put('/:id', async (req, res) => {
     });
     res.json(updatedRemedy);
   } catch (error) {
-    res.status(500).json({ error: 'Erro ao atualizar o medicamento' });
+    res.status(500).json({  error: error });
   }
 });
 
@@ -84,7 +84,7 @@ router.delete('/:id', async (req, res) => {
     });
     res.json({ message: 'Medicamento deletado com sucesso' });
   } catch (error) {
-    res.status(500).json({ error: 'Erro ao deletar o medicamento' });
+    res.status(500).json({  error: error });
   }
 });
 
